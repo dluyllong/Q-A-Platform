1bbd5867f1d8082e761b5a1d38f57f614e964ebb
HoaNguyen <thaihoa311@gmail.com>
2014-04-29 17:09:38 +0700

    Modified   app/scripts/directives.js

--- app/scripts/directives.js
+++ app/scripts/directives.js
@@ -10,51 +10,58 @@
 
 var Directives = angular.module('QAP.directives', ['QAP.filters']);
 
-Directives.directive('smarttime', [ '$timeout', '$filter', function ($timeout, $filter) {
-	return function(scope, element, attrs) {
-        var type = attrs.smarttime,
-			intervalLength = 1000,
-			filter = $filter('smartDateTime');
-			
-        function updateTime() {
-			element.text(filter(scope[type].date));
-        }
+Directives.directive('smarttime', ['$timeout', '$filter',
+	function($timeout, $filter) {
+		return function(scope, element, attrs) {
+			var type = attrs.smarttime,
+				intervalLength = 1000,
+				filter = $filter('smartDateTime');
 
-        function updateLater() {
-			timeoutId = $timeout(function() {
-				updateTime();
-				updateLater();
-			}, intervalLength);
-        }
+			function updateTime() {
+				element.text(filter(scope[type].date));
+			}
 
-        element.bind('$destroy', function() {
-			$timeout.cancel(timeoutId);
-        });
+			function updateLater() {
+				timeoutId = $timeout(function() {
+					updateTime();
+					updateLater();
+				}, intervalLength);
+			}
 
-        updateTime();
-        updateLater();
-	};
-}]);
+			element.bind('$destroy', function() {
+				$timeout.cancel(timeoutId);
+			});
+
+			updateTime();
+			updateLater();
+		};
+	}
+]);
 
 Directives.directive('myBackToTop', function() {
-    function link(scope, element, attrs) {
-    	element.on('click', function() {
-		    if(navigator.appName!='Opera'){
-		    	$('html, body').animate({ scrollTop: 0 }, 1000)}
-	 		else{$('html').animate({ scrollTop: 0 }, 1000)}
-	    });
-    }
-    return {
-      link: link
-    };
-});
-window.onscroll = function (event) {
-	var heightWindow = $(window).height();
-	
-	if($(this).scrollTop()>=heightWindow){
-      	$('.btn-back-to-top').css('display','inline-block')
-    }
-	else{
-		$('.btn-back-to-top').css('display','none')
+	function link(scope, element, attrs) {
+		element.on('click', function() {
+			if (navigator.appName != 'Opera') {
+				$('html, body').animate({
+					scrollTop: 0
+				}, 1000)
+			} else {
+				$('html').animate({
+					scrollTop: 0
+				}, 1000)
+			};
+			window.onscroll = function(event) {
+				var heightWindow = $(window).height();
+
+				if ($(this).scrollTop() >= heightWindow) {
+					$('.btn-back-to-top').css('display', 'inline-block')
+				} else {
+					$('.btn-back-to-top').css('display', 'none')
+				}
+			}
+		});
 	}
-}
+	return {
+		link: link
+	};
+});
